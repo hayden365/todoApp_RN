403ece579efaccd3cd0144c3c6221519d3e4dc34
hayden365 <ysmh100@gmail.com>
2025-07-17 22:44:37 +0900

    feat: setting up backend with Covex

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,32 @@
-import { StyleSheet, Text, View } from "react-native";
+import { api } from "@/convex/_generated/api";
+import useTheme from "@/hooks/useTheme";
+import { useMutation, useQuery } from "convex/react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+  const { toggleDarkMode } = useTheme();
+
+  const todos = useQuery(api.todos.getTodos);
+  // console.log(todos);
+
+  const addTodo = useMutation(api.todos.addTodo);
+  const clearAllTodos = useMutation(api.todos.clearAllTodos);
+
   return (
     <View style={styles.container}>
       <Text style={styles.content}>
         Edit app/index.tsx to edit this screen.
       </Text>
       <Text>hi</Text>
+      <TouchableOpacity onPress={toggleDarkMode}>
+        <Text>toggle the mode</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => addTodo({ text: "walk the dog" })}>
+        <Text>Add a new todo</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => clearAllTodos()}>
+        <Text>Clear all</Text>
+      </TouchableOpacity>
     </View>
   );
 }
